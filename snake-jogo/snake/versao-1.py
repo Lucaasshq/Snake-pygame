228f5f0f84c05da51a48a1f5e14c78fc61cd7ff6
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2025-01-30 18:34:02 -0300

    initial commit

--- snake-jogo/snake/versao-1.py
+++ snake-jogo/snake/versao-1.py
@@ -7,23 +7,63 @@ largura, altura = 640, 480
 tamanho = largura, altura
 tela = pygame.display.set_mode((tamanho))
 
-imagem_fundo = pygame.image.load("snake-jogo\snake\Fundo.png")
 nome_do_jogo = pygame.display.set_caption("Snake")
+cor_fundo = pygame.Color("blue") 
+
+cobra_pos = [100, 50]
+cobra_corpo = [[100, 50], [90, 50], [80, 50]]
 
 
-cor_fundo = (255, 0, 0) 
+
+direcao = "DIREITA"
+
+mudar_direcao = direcao
+fps = pygame.time.Clock()
+
 
 rodando = True
 while rodando:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             rodando = False
-
-
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                mudar_direcao = 'CIMA'
+            elif event.key == pygame.K_DOWN:
+                mudar_direcao = 'BAIXO'
+            elif event.key == pygame.K_LEFT:
+                mudar_direcao = 'ESQUERDA'
+            elif event.key == pygame.K_RIGHT:
+                mudar_direcao = 'DIREITA'
+                
+    if mudar_direcao == 'CIMA' and direcao != 'BAIXO':
+        direcao = 'CIMA'
+    if mudar_direcao == 'BAIXO' and direcao != 'CIMA':
+        direcao = 'BAIXO'
+    if mudar_direcao == 'ESQUERDA' and direcao != 'DIREITA':
+        direcao = 'ESQUERDA'
+    if mudar_direcao == 'DIREITA' and direcao != 'ESQUERDA':
+        direcao = 'DIREITA'
+        
+    if direcao == 'CIMA':
+        cobra_pos[1] -= 10
+    if direcao == 'BAIXO':
+        cobra_pos[1] += 10
+    if direcao == 'ESQUERDA':
+        cobra_pos[0] -= 10
+    if direcao == 'DIREITA':
+        cobra_pos[0] += 10
+        
+    cobra_corpo.insert(0, list(cobra_pos))
+    cobra_corpo.pop()
     
+    tela.fill(cor_fundo)
     
-    tela.blit(imagem_fundo, (0, 0))
- 
-    pygame.display.flip()
+    for corpo in cobra_corpo:
+        pygame.draw.rect(tela, pygame.Color('white'), pygame.Rect(corpo[0], corpo[1], 10, 10))
+        
+    pygame.display.update()
+    fps.tick(15)
+pygame.quit()
         
 
---
+++ snake-jogo/snake/versao2-.py
@@ -0,0 +1,81 @@
+import pygame
+import sys
+import random
+
+pygame.init()
+
+largura, altura = 640, 480
+tamanho = largura, altura
+tela = pygame.display.set_mode((tamanho))
+
+nome_do_jogo = pygame.display.set_caption("Snake")
+cor_fundo = pygame.Color("blue") 
+
+cobra_pos = [100, 50]
+cobra_corpo = [[100, 50], [90, 50], [80, 50]]
+
+fruta_pos = [random.randrange(1, (largura // 10)) * 10, random.randrange(1, (altura // 10)) * 10]
+fruta_spawn = True
+
+direcao = "DIREITA"
+
+mudar_direcao = direcao
+fps = pygame.time.Clock()
+
+
+rodando = True
+while rodando:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            rodando = False
+        elif event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_UP:
+                mudar_direcao = 'CIMA'
+            elif event.key == pygame.K_DOWN:
+                mudar_direcao = 'BAIXO'
+            elif event.key == pygame.K_LEFT:
+                mudar_direcao = 'ESQUERDA'
+            elif event.key == pygame.K_RIGHT:
+                mudar_direcao = 'DIREITA'
+                
+    if mudar_direcao == 'CIMA' and direcao != 'BAIXO':
+        direcao = 'CIMA'
+    if mudar_direcao == 'BAIXO' and direcao != 'CIMA':
+        direcao = 'BAIXO'
+    if mudar_direcao == 'ESQUERDA' and direcao != 'DIREITA':
+        direcao = 'ESQUERDA'
+    if mudar_direcao == 'DIREITA' and direcao != 'ESQUERDA':
+        direcao = 'DIREITA'
+        
+    if direcao == 'CIMA':
+        cobra_pos[1] -= 10
+    if direcao == 'BAIXO':
+        cobra_pos[1] += 10
+    if direcao == 'ESQUERDA':
+        cobra_pos[0] -= 10
+    if direcao == 'DIREITA':
+        cobra_pos[0] += 10
+        
+    cobra_corpo.insert(0, list(cobra_pos))
+    if cobra_pos == fruta_pos:
+        fruta_spawn = False
+    else:
+        cobra_corpo.pop()
+        
+    if not fruta_spawn:
+        fruta_pos = [random.randrange(1, (largura // 10)) * 10, random.randrange(1, (altura // 10)) * 10]
+    fruta_spawn = True
+    
+    tela.fill(cor_fundo)
+    
+    for corpo in cobra_corpo:
+        pygame.draw.rect(tela, pygame.Color('white'), pygame.Rect(corpo[0], corpo[1], 10, 10))
+        
+    pygame.draw.rect(tela, pygame.Color("red"), pygame.Rect(fruta_pos[0], fruta_pos[1], 10, 10))
+
+        
+    pygame.display.update()
+    fps.tick(15)
+pygame.quit()
+        
+
