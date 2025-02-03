13821bb22a0723ce13ec3f7136497683ebc787e4
LUCAS HENRIQUE BARROS MEDEIROS <20242014040042@ifrn.local>
2025-02-03 15:42:07 -0300

    novas funcionalidades

--- snake-jogo/snake/versao2-.py
+++ snake-jogo/snake/versao2-.py
@@ -1,47 +1,63 @@
+
 import pygame
-import sys
 import random
 
-# Inicializa o módulo pygame
+
 pygame.init()
 
-# Define a largura e altura da tela
+
 largura, altura = 640, 480
 tamanho = largura, altura
 # Cria a tela do jogo com as dimensões especificadas
 tela = pygame.display.set_mode((tamanho))
 
-# Define o título da janela do jogo
+
 nome_do_jogo = pygame.display.set_caption("Snake")
-# Define a cor de fundo da tela
+
 cor_fundo = pygame.Color("blue") 
 
-# Posição inicial da cabeça da cobra
+
 cobra_pos = [100, 50]
-# Corpo inicial da cobra (uma lista de segmentos)
+
 cobra_corpo = [[100, 50], [90, 50], [80, 50]]
 
 # Gera a posição inicial da fruta de forma aleatória
 fruta_pos = [random.randrange(1, (largura // 10)) * 10, random.randrange(1, (altura // 10)) * 10]
 fruta_spawn = True
 
-# Direção inicial da cobra
-direcao = "DIREITA"
+def exibir_texto_pontos(pontos):
+    text_color = (255,255,255)
+    font = pygame.font.Font(None, 30)
+    text_surface = font.render(f"Pontos: {pontos}", True, text_color)
+    text_position = text_surface.get_rect(center=(50,10))
 
-# Variável para armazenar a nova direção da cobra
+    return text_surface, text_position
+
+def carregar_imagem_fruta():
+    imagem_fruta = pygame.image.load("snake-jogo\snake\Maçã.png")
+    imagem_fruta = pygame.transform.scale(imagem_fruta, (10, 10))
+    return imagem_fruta
+
+imagem_fruta = carregar_imagem_fruta()
+
+
+direcao = "DIREITA"
+pontos = 0
+
+
 mudar_direcao = direcao
-# Controla os frames por segundo (FPS) do jogo
+
 fps = pygame.time.Clock()
 
-# Loop principal do jogo
+
 rodando = True
 while rodando:
-    # Verifica os eventos (como pressionar teclas ou fechar a janela)
+   
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             rodando = False
         elif event.type == pygame.KEYDOWN:
-            # Muda a direção com base na tecla pressionada
+        
             if event.key == pygame.K_UP:
                 mudar_direcao = 'CIMA'
             elif event.key == pygame.K_DOWN:
@@ -73,9 +89,10 @@ while rodando:
         
     # Insere a nova posição da cabeça da cobra no início do corpo
     cobra_corpo.insert(0, list(cobra_pos))
-    # Verifica se a cobra comeu a fruta
+    
     if cobra_pos == fruta_pos:
         fruta_spawn = False
+        pontos += 1
     else:
         # Remove o último segmento do corpo da cobra
         cobra_corpo.pop()
@@ -87,18 +104,20 @@ while rodando:
     
     # Preenche a tela com a cor de fundo
     tela.fill(cor_fundo)
+    text_surface, text_position = exibir_texto_pontos(pontos)
+    tela.blit(text_surface, text_position)
     
-    # Desenha cada segmento do corpo da cobra na tela
+    
     for corpo in cobra_corpo:
         pygame.draw.rect(tela, pygame.Color('white'), pygame.Rect(corpo[0], corpo[1], 10, 10))
         
-    # Desenha a fruta na tela
-    pygame.draw.rect(tela, pygame.Color("red"), pygame.Rect(fruta_pos[0], fruta_pos[1], 10, 10))
+    
+    tela.blit(imagem_fruta, (fruta_pos[0], fruta_pos[1]))
 
-    # Atualiza a tela
+    
     pygame.display.update()
-    # Define a taxa de atualização do jogo (FPS)
+    
     fps.tick(15)
 
-# Encerra o módulo pygame
+
 pygame.quit()
